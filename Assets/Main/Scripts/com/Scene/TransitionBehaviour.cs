2b036a4a22ddcd0340e613b825b71aba95677431
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-06-22 12:15:06 +0800

    wip

---
+++ Assets/Main/Scripts/com/Scene/TransitionBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System;
+using DG.Tweening;
+
+namespace com
+{
+    public class TransitionBehaviour : MonoBehaviour
+    {
+        public float sizeMin;
+        public float sizeMax;
+        public float duration;
+        public float durationSmall;
+        public float rotationDelta;
+
+        public CanvasGroup cg;
+        public RectTransform view;
+        private Action _cb;
+
+        public static TransitionBehaviour instance { get; private set; }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Start()
+        {
+            Hide();
+        }
+
+        public void SetCallback(Action cb = null)
+        {
+            _cb = cb;
+        }
+
+        public void Hide()
+        {
+            //Debug.Log("Hide");
+            cg.alpha = 0;
+            SetCallback();
+        }
+
+        public void ShowBigger(Action cb = null)
+        {
+            //Debug.Log("ShowBigger");
+            StartTransition(sizeMin, sizeMax, duration, cb);
+        }
+
+        public void ShowSmaller(Action cb = null)
+        {
+            //Debug.Log("ShowSmaller");
+            StartTransition(sizeMax, sizeMin, durationSmall, cb);
+        }
+
+        void StartTransition(float start, float end, float t, Action cb)
+        {
+            //Debug.Log("StartTransition");
+            SetCallback(cb);
+            view.localScale = new Vector3(start, start, 1);
+            view.DOKill();
+            view.DOScale(new Vector3(end, end, 1), t).SetEase(Ease.InOutCubic);
+            view.DORotate(new Vector3(0, 0, rotationDelta), t, RotateMode.FastBeyond360).SetEase(Ease.InOutCubic).OnComplete(() =>
+            {
+                //Debug.Log("cb");
+                _cb?.Invoke();
+                //Hide();
+            });
+            cg.alpha = 1;
+        }
+    }
+}
