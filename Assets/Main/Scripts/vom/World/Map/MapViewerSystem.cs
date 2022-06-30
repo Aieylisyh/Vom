c8b46b348781727295bb0cdc7789024c99616324
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-06-30 10:10:53 +0800

    viewer

---
+++ Assets/Main/Scripts/vom/World/Map/MapViewerSystem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace vom
+{
+    public class MapViewerSystem : MonoBehaviour
+    {
+        public MapItem item;
+        public Transform tilesParent;
+
+        void Start()
+        {
+            foreach (var tile in item.tiles)
+            {
+                Visualize(tile);
+            }
+        }
+
+
+        public void Visualize(TileCacheBehaviour.OutputTileData tileData)
+        {
+            bool hasGround = (tileData.tile != null);
+            if (hasGround)
+            {
+                CreateGroundView(tileData);
+            }
+
+            bool hasObstacle = (tileData.obstacle != null);
+            if (hasObstacle)
+            {
+                CreateObstacleView(tileData);
+            }
+        }
+
+        void CreateObstacleView(TileCacheBehaviour.OutputTileData tileData)
+        {
+            var obstacleView = Instantiate(tileData.obstacle, tilesParent);
+            obstacleView.transform.localPosition = new Vector3(tileData.x, tileData.height, tileData.z);
+        }
+
+        void CreateGroundView(TileCacheBehaviour.OutputTileData tileData)
+        {
+            var groundView = Instantiate(tileData.tile, tilesParent);
+            groundView.transform.localPosition = new Vector3(tileData.x, tileData.height, tileData.z);
+        }
+    }
+}
