b667bc415a118ffa1f8dcf77e040b57cc730154e
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-06-30 11:40:18 +0800

    wip

---
+++ Assets/Main/Scripts/vom/World/Props/PhysicsPropsBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace vom
+{
+    public class PhysicsPropsBehaviour : game.Ticker
+    {
+        Vector3 _toPlayerDir;
+        public Rigidbody rb;
+
+        public bool IsInPlayerView()
+        {
+            var dX = _toPlayerDir.x;
+            var dZ = _toPlayerDir.z;
+            //Debug.Log(dX + " " + dZ + " | " + MapSystem.instance.tileNumRight + " " + MapSystem.instance.tileNumForward + " " + MapSystem.instance.tileNumBackward);
+            if (Mathf.Abs(dX) < MapSystem.instance.tileNumRight)
+            {
+                if (-dZ < MapSystem.instance.tileNumForward && dZ < MapSystem.instance.tileNumBackward)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Update is called once per frame
+        protected override void Tick()
+        {
+            base.Tick();
+            var playerPos = PlayerBehaviour.instance.transform.position;
+            _toPlayerDir = playerPos - transform.position;
+            var inView = IsInPlayerView();
+            if (inView)
+            {
+                if (!rb.useGravity)
+                {
+                    rb.useGravity = true;
+                    rb.isKinematic = false;
+                    rb.constraints = RigidbodyConstraints.None;
+                }
+            }
+            else
+            {
+                if (rb.useGravity)
+                {
+                    rb.useGravity = false;
+                    rb.isKinematic = true;
+                    rb.constraints = RigidbodyConstraints.FreezeAll;
+                }
+            }
+        }
+    }
+}
